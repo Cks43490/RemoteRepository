8145e299bc12cb3cfd7ea697e3c685ba614bd176
Dell <Dell@Hancock>
2017-05-07 16:49:06 +0530

    changed by vikash

--- HelloWorldProject/src/com/nt/test/Test.java
+++ HelloWorldProject/src/com/nt/test/Test.java
@@ -3,10 +3,12 @@ package com.nt.test;
 public class Test {
 
 	public static void main(String[] args) {
+		int i=10;
 		System.out.println("hello to the world of gitHub");
 		System.out.println("Changed By Vikash");
 		System.out.println("hello mere bhai vikash");
 		System.out.println("656565");
+		System.out.println(i);
 		
 
 	}
